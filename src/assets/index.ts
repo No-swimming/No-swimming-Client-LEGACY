9e78aa193efb9c83a9e70863e2ab59bc373dd01e
kimgyuha <kimgyuha25@gmail.com>
2023-01-16 21:54:02 +0900

    add : 이미지 파일, 테마 파일
    헤더에 필요한 이미지 파일을 export하여 나중에 손쉽게 가져올 수 있게 해줌
    테마를 설정하여 나중에 색을 편리하게 바꿀 수 있도록 함

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+/* 헤더 이미지 */
+export const LogoImg = require("./imgs/Logo.png");
+export const BestSeller = require("./imgs/BestSeller.png");
+export const BookShelf = require("./imgs/BookShelf.png");
+export const MyBook = require("./imgs/MyBook.png");
+// 임시 사용자 이미지
+export const Profile = require("./imgs/Profile.png");
