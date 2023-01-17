14f13ff4efac8835f951ed17a5e7c44e35f42eef
kimgyuha <kimgyuha25@gmail.com>
2023-01-17 20:59:08 +0900

    add : 이미지 파일
    화면에 배경 책 이미지, 로고, 검색 버튼 이미지 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,13 @@
-/* 헤더 이미지 */
 export const LogoImg = require("./imgs/Logo.png");
+/* 헤더 이미지 */
 export const BestSeller = require("./imgs/BestSeller.png");
 export const BookShelf = require("./imgs/BookShelf.png");
 export const MyBook = require("./imgs/MyBook.png");
 // 임시 사용자 이미지
 export const Profile = require("./imgs/Profile.png");
+
+/* 배경 책 이미지 */
+export const Background = require("./imgs/Background.png");
+
+/* 검색바 이미지 */
+export const SearchBtn = require("./imgs/Search.png");
