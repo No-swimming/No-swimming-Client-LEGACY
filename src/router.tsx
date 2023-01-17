86a164736ba6b13fc63fee4b0def48a8ef0268aa
kimgyuha <kimgyuha25@gmail.com>
2023-01-17 21:00:21 +0900

    feat : router 수정
    "/" 를 메인페이지 url로 설정함
    문제점 : header를 적용하면 페이지가 눈에 보이지 않음

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import Main from "./pages/Main";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Header />} />
+        <Route path="/" element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
