9ec02343a9e2a624e298c1ca0262a9a002f87093
김상구 <spair0039@gmail.com>
2023-01-20 04:37:57 +0900

    add : 배경 콘테이너 flex-dir 속성 추가

--- src/styles/Login.tsx
+++ src/styles/Login.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const BgContainer = styled.div`
     display: flex;
+    flex-direction: column;
     align-items: center;
     padding: 32px;
     gap: 32px;
