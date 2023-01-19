0531b7694f1581d259e793cdeee6b99520732f4f
김상구 <spair0039@gmail.com>
2023-01-20 04:49:40 +0900

    add : 구글 로그인버튼 컴포넌트 style

--- src/styles/Login.tsx
+++ src/styles/Login.tsx
@@ -8,6 +8,30 @@ export const BgContainer = styled.div`
     gap: 32px;
     border-radius: 16px;
 
+    font-size: 24px;
+
     background-color: #FFFFFF;
     box-shadow: 0px 0px 32px rgba(0, 0, 0, 0.1);
+`;
+
+export const GoogleBtn = styled.button`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 10px;
+
+    padding: 8px 16px;
+
+    background-color: #E6E6E6E6;
+    border-radius: 888px;
+    border: none;
+
+    font-size: 20px;
+
+    cursor: pointer;
+        
+    & > img{
+        width: 18px;
+        height: 18px;
+    }
 `;
