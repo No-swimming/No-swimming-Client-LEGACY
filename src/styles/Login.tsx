b1be424b6007a525efc6ee3da68e254186aa66dd
김상구 <spair0039@gmail.com>
2023-01-20 04:34:56 +0900

    add : 배경 콘테이너 제작

---
+++ src/styles/Login.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const BgContainer = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 32px;
+    gap: 32px;
+    border-radius: 16px;
+
+    background-color: #FFFFFF;
+    box-shadow: 0px 0px 32px rgba(0, 0, 0, 0.1);
+`;
