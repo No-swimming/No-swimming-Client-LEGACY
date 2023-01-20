5dcaf1af29c40cdc5b9bc3d4729fcf80fdf2e7da
김상구 <spair0039@gmail.com>
2023-01-21 00:42:45 +0900

    add : 인풋 컴포넌트 개발

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,31 @@
 import * as _ from "../styles/Login";
 import { GoogleMiniIcn } from "../assets";
+import { Eye28Regular } from "@fluentui/react-icons";
 
 function GoogleLoginBtn(){
-        return(
-            <_.GoogleBtn>
-                <img src={GoogleMiniIcn}/>
-                Google로 로그인
-            </_.GoogleBtn>
+    return(
+        <_.GoogleBtn>
+            <img src={GoogleMiniIcn}/>
+            Google로 로그인
+        </_.GoogleBtn>
     );
 };
 
-export {GoogleLoginBtn};
+type LoginInputType = {
+    label: string;
+    isPassword?: boolean;
+}
+
+function LoginInput({label, isPassword}:LoginInputType){
+    return(
+        <_.Input>
+            <label>{label}</label>
+            <div>
+                {isPassword ? <input type="password"/> : <input type="text"/> }
+                {isPassword ? <Eye28Regular/> : null}
+            </div>
+        </_.Input>
+    );
+};
+
+export { GoogleLoginBtn, LoginInput };
