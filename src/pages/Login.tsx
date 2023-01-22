74298f4da21f46e4039185a5589461220c88f828
김상구 <spair0039@gmail.com>
2023-01-22 17:31:37 +0900

    add : 로그인 컴포넌트로 전환

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import GoogleLoginBtn from "../components/Login";
+import { GoogleLoginBtn, LoginInput } from "../components/Login";
 import * as _ from "../styles/Login";
 
 const Login = () => {
@@ -10,6 +10,7 @@ const Login = () => {
         else if(cardState === 'admin') setCardState('google');
     }
 
+    //구글(학생)로그인
     if(cardState==='google'){
         return (
             <_.BgContainer>
@@ -18,23 +19,21 @@ const Login = () => {
                 <a onClick={switchCard}>관리자 로그인</a>
             </_.BgContainer>
     );}
+
+    //관리자 로그인
     else{
         return(
             <_.BgContainer>
-                <p>관리자 로그인</p>
-                <a onClick={switchCard}>← 돌아가기</a>
                 <div>
-                    <label>아이디</label>
-                    <input></input>
+                    <p>관리자 로그인</p>
+                    <a onClick={switchCard}>← 돌아가기</a>
                 </div>
-                <div>
-                    <label>비밀번호</label>
-                    <input></input>
-                </div>
-                계정을 받으려면 어쩌구
-                <button>로그인</button>
+                <LoginInput label="아이디" />
+                <LoginInput label="비밀번호" isPassword />
+                계정을 발급받으려면 서비스 관리자에게 문의하세요
+                <_.BtnLong>로그인</_.BtnLong>
             </_.BgContainer>
-    );
+        );
     }
 };
 
