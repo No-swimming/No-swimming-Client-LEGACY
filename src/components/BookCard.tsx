e7e6595c31c605725c51b3bdb91ed13aea63931c
김상구 <spair0039@gmail.com>
2023-01-28 16:47:11 +0900

    add : 기본 디자인(임시)

---
+++ src/components/BookCard.tsx
@@ -0,0 +1,30 @@
+import { Heart24Regular } from '@fluentui/react-icons';
+import * as _ from '../styles/BookCard';
+
+function CardLarge(){
+    const imageURL:string = "https://asfadsfasdfa";
+
+    return(
+        <_.CardBg>
+            <img src='https://shopping-phinf.pstatic.net/main_3429447/34294472620.20230119071329.jpg?type=w300'/>
+            <div>
+                <_.CardTitle>아버지의 해방일지</_.CardTitle>
+                <_.CardTitle className='trans'>정지아 | 2022</_.CardTitle>
+                <_.CardBody>새삼스럽게 경탄스럽다! 압도적인 몰입감, 가슴 먹먹한 감동 정지아의 손끝에서 펼쳐지는 시대의 온기 미스터리 같은 한 남...</_.CardBody>
+                <div>
+                    <_.ButtonBlack className='icon'>
+                        <Heart24Regular primaryFill='#ffffff'/>
+                    </_.ButtonBlack>
+                    <_.ButtonBlack>
+                        읽은 책에 추가
+                    </_.ButtonBlack>
+                    <_.ButtonBlack>
+                        독서록 작성
+                    </_.ButtonBlack>
+                </div>
+            </div>
+        </_.CardBg>
+    )
+}
+
+export default CardLarge;
---
+++ src/styles/BookCard.tsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const CardBg = styled.div`
+    display:flex;
+    gap: 12px;
+    padding: 12px;
+    box-sizing: border-box;
+    border-radius: 12px;
+    background: #2D287B;
+    color: #FFFFFF;
+    width: 650px;
+    height: 200px;
+    & > div > div{
+        display:flex;
+        gap: 8px;
+        align-items: center;
+    }
+    & > img{
+        width: 124px;
+        border-radius:4px;
+    }
+`;
+
+export const CardTitle = styled.p`
+    font-weight: 700;
+    font-size: 24px;
+    &.trans{
+        opacity: 0.5;
+    }
+`;
+
+export const CardBody = styled.p`
+    font-weight: 400;
+    font-size: 20px;
+    opacity: 0.5;
+`;
+
+export const ButtonBlack = styled.button`
+    display:flex;
+
+    padding: 8px 16px;
+    background: #0000007f;
+    border-radius: 99px;
+    font-size: 20px;
+    color: #FFFFFF;
+    border: none;
+    &.icon{
+        padding: 8px 8px;
+    }
+`;
