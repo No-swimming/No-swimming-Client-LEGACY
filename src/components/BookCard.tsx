5d3d5ca0517761034c4e4c297cd45e8379c012cd
김상구 <spair0039@gmail.com>
2023-02-02 16:49:06 +0900

    add : 하트 기능

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,15 +1,16 @@
-import { Heart24Regular } from '@fluentui/react-icons';
+import { Heart24Filled, Heart24Regular } from '@fluentui/react-icons';
 import axios from 'axios';
 import * as _ from '../styles/BookCard';
 
 type cardType = {
     isbn: string;
+    hearted?: boolean;
 }
 
-function CardLarge({isbn}:cardType){
+function CardLarge({isbn, hearted}:cardType){
     const axiosConf = {
         method: 'get',
-        url: 'https://openapi.naver.com/v1/search/book_adv.json?d_isbn=9788936438838',
+        url: `https://openapi.naver.com/v1/search/book_adv.json?d_isbn=?{isbn}`,
         headers: { 
             'X-Naver-Client-Id': 'key', 
             'X-Naver-Client-Secret': 'secret'
@@ -33,7 +34,7 @@ function CardLarge({isbn}:cardType){
                 <_.CardBody>새삼스럽게 경탄스럽다! 압도적인 몰입감, 가슴 먹먹한 감동 정지아의 손끝에서 펼쳐지는 시대의 온기 미스터리 같은 한 남...</_.CardBody>
                 <div>
                     <_.ButtonBlack className='icon'>
-                        <Heart24Regular primaryFill='#ffffff'/>
+                        {hearted ? <Heart24Filled primaryFill='#ff0000'/> : <Heart24Regular primaryFill='#ffffff'/>}
                     </_.ButtonBlack>
                     <_.ButtonBlack>
                         읽은 책에 추가
