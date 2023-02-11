195a4e5b1f1157a40d106310cb7e9485c058fa35
kimgyuha <kimgyuha25@gmail.com>
2023-02-11 23:35:51 +0900

    add : 검색바 페이지 이동 기능 추가

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,14 +1,30 @@
 import { Search32Regular } from "@fluentui/react-icons";
 import { SearchBtn } from "../assets";
 import * as _ from "../styles/Main";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Search = () => {
+  const navigate = useNavigate();
+  const [searchInput, setSearchInput] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value);
+  }
+  const searchBook = () => {
+    if (searchInput === "") alert("검색어를 입력해주세요")
+    else navigate(`/search/${searchInput}`)
+  }
+  const onClickEvent = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key == "Enter") {
+      searchBook();
+    }
+  }
   const data = ["쥰내편한 편의점", "동물농장", "정의란 이것이다"];
   return (
     <_.SearchContainer>
       <_.SearchInput>
-        <input placeholder="제목, 작가 검색" />
-        <button>
+        <input placeholder="제목, 작가 검색" value={searchInput} onChange={onChange} onKeyDown={onClickEvent} />
+        <button onClick={searchBook}>
           <Search32Regular primaryFill="#BFBFBF" />
         </button>
       </_.SearchInput>
