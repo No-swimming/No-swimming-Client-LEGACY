b4790efb981183181c3f74081a2aee4ff9c0200a
김상구 <spair0039@gmail.com>
2023-02-11 16:51:42 +0900

    add : 카드 연동 표시 코드(이론상)

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -39,13 +39,14 @@ function CardLarge({isbn, hearted}:cardType){
     axios(axiosConf)
         .then(function (response) {
         naverBookResponse = response.data.items[0];
+        console.log(naverBookResponse)
     })
         .catch(function (error) {
         console.log(error);
     });
 
     function getAverage(){
-        average('http://monotype.iptime.org:10888/https://shopping-phinf.pstatic.net/main_3429447/34294472620.20230119071329.jpg?type=w300',
+        average(`http://monotype.iptime.org:10888/${naverBookResponse.image}`,
             {format:"hex"}
         )
         .then(color => setBgcolor(color as string));
@@ -53,10 +54,10 @@ function CardLarge({isbn, hearted}:cardType){
 
     return(
         <_.CardBg backgroud={bgcolor}>
-            <img src='https://shopping-phinf.pstatic.net/main_3429447/34294472620.20230119071329.jpg?type=w300' onLoad={getAverage} />
+            <img src={naverBookResponse.image} onLoad={getAverage} />
             <div>
-                <_.CardTitle>아버지의 해방일지</_.CardTitle>
-                <_.CardTitle className='trans'>정지아 | 2022</_.CardTitle>
+                <_.CardTitle>{naverBookResponse.title}</_.CardTitle>
+                <_.CardTitle className='trans'>{naverBookResponse.author} | {naverBookResponse.pubdate.substr(0,4)}</_.CardTitle>
                 <_.CardBody>새삼스럽게 경탄스럽다! 압도적인 몰입감, 가슴 먹먹한 감동 정지아의 손끝에서 펼쳐지는 시대의 온기 미스터리 같은 한 남...</_.CardBody>
                 <div>
                     <_.ButtonBlack className='icon'>
